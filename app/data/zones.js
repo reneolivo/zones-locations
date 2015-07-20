270e8267627bad304233b302375dcf2acb2c3613
Rene Olivo <minet.ws@gmail.com>
2015-07-20 19:26:15 -0400

    change zones structure

--- app/data/zones.js
+++ app/data/zones.js
@@ -3,6 +3,44 @@ define([
 		'../../data/level-2-zones',
 		'../../data/level-3-zones'
 	], function module(zones1, zones2, zones3) {
-		return zones1;
+		var zones = [];
+
+		function findZoneChildren(zones, id) {
+			var children = [];
+
+			for(var i in zones) {
+				if (zones[i][2] === id)
+					children.push(zones[i]);
+			}
+
+			return children;
+		}
+
+		for(var i in zones1) {
+			zones.push({
+				name: zones1[i][1],
+				level: 1
+			});
+
+			var filteredZones2 = findZoneChildren(zones2, zones1[i][0]);
+
+			for(var ii in filteredZones2) {
+				zones.push({
+					name: filteredZones2[ii][1] + ', ' + zones1[i][1],
+					level: 2
+				});
+
+				var filteredZones3 = findZoneChildren(zones3, filteredZones2[ii][0]);
+
+				for(var iii in filteredZones3) {
+					zones.push({
+						name: filteredZones3[iii][1] + ', ' + filteredZones2[ii][1] + ', ' + zones1[i][1],
+						level: 3
+					});
+				}
+			}
+		}
+
+		return zones;
 	}
 );
