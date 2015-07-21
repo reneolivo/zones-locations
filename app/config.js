863781df4a0e5432c4cbd61d0a117e765d5d523b
Rene Olivo <minet.ws@gmail.com>
2015-07-20 21:37:15 -0400

    add google maps

--- app/config.js
+++ app/config.js
@@ -1,5 +1,6 @@
 require.config({
 	paths: {
+		async: '../bower_components/requirejs-plugins/src/async',
 		knockout: '../bower_components/knockout/dist/knockout'
 	}
 });
--- app/main.js
+++ app/main.js
@@ -1,9 +1,36 @@
 require([
 		'knockout',
+		'async!http://maps.google.com/maps/api/js',
 		'./data/zones'
-	], function module(ko, zones) {
+	], function module(ko, GoogleMaps, zones) {
+		var map = new google.maps.Map(document.getElementById("zones-location-map"), {
+			center:new google.maps.LatLng(51.508742,-0.120850),
+			zoom: 8,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		});
+
+		var geo = new google.maps.Geocoder();
+
 		var ui = {
-			zones: zones
+			zones: zones,
+			selectedZone: ko.observable(),
+
+
+			selectZone: function selectZone() {
+				var zone = this;
+
+				ui.selectedZone(zone);
+
+				geo.geocode({
+						address: zone.name
+					}, function response(result, status) {
+						if (status !== google.maps.GeocoderStatus.OK)
+							return alert('No address found');
+
+						console.log(result);
+					}
+				);
+			}
 		};
 
 		ko.applyBindings(ui);
